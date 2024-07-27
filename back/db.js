4edd28bb3fd57dd64c1b7dd6441d43ff54c8b43f
hema2820 <email@example.com>
2024-07-27 15:38:57 +0530

    updated

--- back/db.js
+++ back/db.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "system",
-  password: "sahyadri@123",
+  password: "",   //user password
   database: "mms" 
 });
 
